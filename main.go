3213f44989c4c346bb4a713f45ec72f16ee43fc2
TanakAiko <cherifmbaye02@gmail.com>
2024-07-09 02:38:32 +0000

    Update port setting

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	conf "post/config"
 	hd "post/internals/handlers"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	port := "8082"
 	http.HandleFunc("/", hd.MainHandler)
-	log.Printf("Server (portAPI) started at http://localhost:%v\n", port)
-	http.ListenAndServe(":"+port, nil)
+	log.Printf("Server (portAPI) started at http://localhost:%v\n", conf.Port)
+	http.ListenAndServe(":"+conf.Port, nil)
 }
